7974cae604ec888addecdd833895c03e262890d3
antomys <deqque13@gmail.com>
2020-11-10 21:08:53 +0200

    Started week2J avaC ourse

---
+++ Java Programming Arrays, Lists, and Structured Data/Week2/WordFrequencies.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class WordFrequencies {
+    private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
+
+    public WordFrequencies (ArrayList<String> myWords, ArrayList<Integer> myFreqs) {
+        this.myFreqs = myFreqs;
+        this.myWords = myWords;
+    }
+
+    public void findUnique() {
+
+    }
+}
